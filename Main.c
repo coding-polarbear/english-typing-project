de177f37673e3d5ec7758b66c9d222e2391a92cc
yeeunHeo <gjdpdms2005@gmail.com>
2018-05-14 18:27:15 +0900

    안녕

--- Main.c
+++ Main.c
@@ -24,27 +24,45 @@ void word()
 //자리
 void place()
 { 
+  int n;
+  int cnt = 1;
+  srand((unsigned)time(NULL));
+
+  n = rand();
+
   char arr1[52];
+  
+  while (1){
   
   for(int i=0; i<26;i++)
   {
 	arr1[i]=65+i;
+	cnt++;
   }
   for(int j=27; j<52;j++)
   {
 	arr1[j]=97+j;
+	cnt++;
   }
   
-  
+while (1) {
+	if (cnt==20)
+		exit(0);
+
+	else 
+		break;
+	}
+  }
+}  
   
 }
 
 int main()
 {
 	int menu;
-	printf("                       영어 타자 연습              \n");
-	printf("	1. 자리수 연습                    2. 낱말 연습\n");
-	printf("	3. 짧은글 연습       			  4. 긴글 연습\n");
+	printf(">> 영문 타자 연습 프로그램 << \n");
+	printf("1. 자리수 연습   2. 낱말 연습\n");
+	printf("3. 짧은글 연습   4. 긴글 연습\n");
 	
 	scanf("%d", &menu);
 	switch(menu)
