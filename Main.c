b1059f3959e4795915a53950261288d2104494b1
배현빈 <baehyeonbin@hotmail.co.kr>
2018-05-28 17:53:14 +0900

    짧은 글 연습 배열 수정

--- Main.c
+++ Main.c
@@ -14,10 +14,14 @@ void initTermios(int echo)
   } else {
       new.c_lflag &= ~ECHO;
   }tcsetattr(0, TCSANOW, &new);
-}void resetTermios(void)
+}
+
+void resetTermios(void)
 {
   tcsetattr(0, TCSANOW, &old);
-}char getch_(int echo){
+}
+
+char getch_(int echo){
   char ch;  initTermios(echo);
   ch = getchar();resetTermios();
   return ch;
@@ -31,23 +35,25 @@ void Init()
 {
 }
 
-void Update(char tmp[], char typing_storge[], int s_prosess, int s_livetype, int s_besttype, int s_acc) 
+void Update(char tmp[], char typing_storge[], int s_prosess, int s_livetype, int s_besttype, int s_acc, int index) 
 {
 	system("clear");
-	 printf("짧은 글 연습\n");
-	 printf("\n");
-		printf("진행도: %d 현재타수: %d 최고타수: %d 정확도: %d \n\n", s_prosess, s_livetype, s_besttype, s_acc);
-		printf("%s\n", tmp);
-		printf("%s", typing_storge);
+	printf("짧은 글 연습\n");
+	printf("\n");
+	printf("진행도: %d 현재타수: %d 최고타수: %d 정확도: %d \n\n", s_prosess, s_livetype, s_besttype, s_acc);
+	printf("%s\n", tmp);
+	for(int i = 0; i <= index; i++) 
+		printf("%c", typing_storge[i]);
+	// printf("%s", typing_storge);
 }
 
 void Render(char tmp[], char title[], int s_prosess, int s_livetype, int s_besttype, int s_acc)
 {
 	system("clear");
 	printf("%s\n", title);
-	 printf("\n");
-		printf("진행도: %d 현재타수: %d 최고타수: %d 정확도: %d \n\n", s_prosess, s_livetype, s_besttype, s_acc);
-		printf("%s\n", tmp);
+	printf("\n");
+	printf("진행도: %d 현재타수: %d 최고타수: %d 정확도: %d \n\n", s_prosess, s_livetype, s_besttype, s_acc);
+	printf("%s\n", tmp);
 }
 
 void placeword(char tmp[], char title[], int s_process, int s_wrong, float s_acc)
@@ -63,22 +69,22 @@ void placeword(char tmp[], char title[], int s_process, int s_wrong, float s_acc
 //짧은글 연습g
 void s_sentence()
 {
-	char tmp[40][200] = ([["Poetry is the mother tongue of mankind"], ["Where there is a will there is a way."],
-	["Think of the end before you begin."], ["Carpe Diem!"],
-	["boys, be ambitious!"], ["Raise your head!"],
-	["Try hard, Try hard!"], ["Ability is decided by one's own effort."],
-	["Faithfulness makes all things possible."], ["Do to others as you would be done by."],
-	["Faith without deeds is useless."], ["The life is only once."],
-	["Think different!"], ["The regret after not doing something is bigger than that of doing something."],
-	["Try your best rather than be the best."], ["I determine my future all by myself."],
-	["All I need to do is move forward!"], ["I must do what I want to do."],
-	["Have fun this time, because it will never come again."], ["My dream don't have forgotten the promise I had until now!!"],
-	["Time is life itself."], ["If  you don't walk today, you will have to run tomorrow."],
-	["Why do you make efforts commonly, don't want to live commonly!"], ["Today, which was proved to be fruitless, is the day that the dead in the past was longing for."],
-	["Never put off for tommorrow what you can do today!"], ["The real effort never betrays me."],
-	["There is no Destiny."], ["Effort is the best way to realizing your dreams."],
-	["No pain, no gain."], ["Opportunity seldom knocks twice."],
-	["Make hay while the sun shines."]]);
+	char tmp[40][200] = {"Poetry is the mother tongue of mankind", "Where there is a will there is a way.",
+	"Think of the end before you begin.", "Carpe Diem!",
+	"boys, be ambitious!", "Raise your head!",
+	"Try hard, Try hard!", "Ability is decided by one's own effort.",
+	"Faithfulness makes all things possible.", "Do to others as you would be done by.",
+	"Faith without deeds is useless.", "The life is only once.",
+	"Think different!", "The regret after not doing something is bigger than that of doing something.",
+	"Try your best rather than be the best.", "I determine my future all by myself.",
+	"All I need to do is move forward!", "I must do what I want to do.",
+	"Have fun this time, because it will never come again.", "My dream don't have forgotten the promise I had until now!!",
+	"Time is life itself.", "If  you don't walk today, you will have to run tomorrow.",
+	"Why do you make efforts commonly, don't want to live commonly!", "Today, which was proved to be fruitless, is the day that the dead in the past was longing for.",
+	"Never put off for tommorrow what you can do today!", "The real effort never betrays me.",
+	"There is no Destiny.", "Effort is the best way to realizing your dreams.",
+	"No pain, no gain.", "Opportunity seldom knocks twice.",
+	"Make hay while the sun shines."};
 
 	char typer, typing_storge[200];
 	int s_prosess = 0, s_livetype = 0, s_besttype = 0, s_acc = 0; // 진행도, 타수, 최고타수, 정확도
@@ -93,10 +99,8 @@ void s_sentence()
 		}
 		else if(typer == 127 || typer == 8) {
 			if(index > 0) {
-			index--;
-			typing_storge[index] = '\0';
-			// printf("\n\b");
-			Update(tmp, typing_storge, index, s_livetype, s_besttype, s_acc);
+				index--;
+				Update(tmp, typing_storge, index, s_livetype, s_besttype, s_acc, index);
 			} else {
 				index = 0;
 				Render(tmp, "짧은 글 연습", index, s_livetype, s_besttype, s_acc);
@@ -106,7 +110,7 @@ void s_sentence()
 		else {
 			typing_storge[index] = typer;
 			index++;
-			Update(tmp, typing_storge, index, s_livetype, s_besttype, s_acc);
+			Update(tmp, typing_storge, index, s_livetype, s_besttype, s_acc, index);
 		}
 	}
 }
@@ -169,22 +173,22 @@ void place()
   float exact =0;
   int error=-1,process=0;
   int count=0;
-	int totalchallenge=0;
+int totalchallenge=0;
   char putalphabet;
 
   while(count<=20)
- {
-   system("clear");
-	 printf ("자리연습 \n");
-   printf("진행도:%d%%  오타수:%d  정확도:%.1f%% ",process,error,exact);
-   printf("\n\n");
-
-  char alphabet[53];
-  for(int i=0; i<26;i++)
   {
-	alphabet[i]=65+i;
-  }
-  for(int j=0; j<26;j++)
+	  system("clear");
+	  printf ("자리연습 \n");
+	  printf("진행도:%d%%  오타수:%d  정확도:%.1f%% ",process,error,exact);
+	  printf("\n\n");
+
+	char alphabet[53];
+	for(int i=0; i<26;i++)
+	{
+		alphabet[i]=65+i;
+	}
+	for(int j=0; j<26;j++)
   {
 	alphabet[26+j]=97+j;
   }
